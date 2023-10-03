5490b86e4508539f614188485ffe571b691af7ed
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2023-10-03 00:43:45 -0300

    a

---
+++ WebForm/Controles/listView_table.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Controles
+{
+    public partial class listView_table : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack == false)
+            {
+                lvwListado.Items.Clear();
+                lvwListado.DataSource = new ArrayList { new { Dni=43432243, Nombre = "Camila"}};
+                lvwListado.DataBind();
+            }
+        }
+    }
+}
