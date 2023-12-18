7a3ac0e0a5a88510ec4b77699abf9f80b4a63831
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2023-12-18 20:53:20 -0300

    .

---
+++ WebForm/Ajax/Formulario.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Formulario.aspx.cs" Inherits="Ajax.Formulario" %>
+
+<asp:Content ID="BodyContent"  ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+         <h3 class="display-4">Formulario</h3>
+        <p class="lead">Algún comentario!</p>
+    </div>
+
+    <asp:Panel ID="panel" ClientIDMode="Static" runat="server">
+        <div id="alerta" ClientIDMode="Static" runat="server" class="alert alert-primary"  style="visibility: hidden;"> </div>
+        <div id="alerta2" ClientIDMode="Static" runat="server" class="alert alert-primary"  > </div>
+
+        <div class="form p-2 m-2">
+            <div class="form-inline col-12">
+                <label class="col-3" for="tbNombre" >Nombre</label>
+                <asp:TextBox ID="tbNombre" ClientIDMode="Static" CssClass="form-control col-8" runat="server"/>
+            </div>
+
+            <div class="text-center">
+                <asp:Button ID="btnPrueba" Text="Prueba" OnClick="btnPrueba_Click" CssClass="btn btn-primary" runat="server"/>
+                <asp:Button ID="btnPrueba2" Text="Prueba2" OnClick="btnPrueba2_Click" CssClass="btn btn-primary" runat="server"/>
+            </div>
+        </div>
+    </asp:Panel>
+
+</asp:Content>
---
+++ WebForm/Ajax/Formulario.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Ajax
+{
+
+
+    public partial class Formulario
+    {
+
+        /// <summary>
+        /// Control panel.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel panel;
+
+        /// <summary>
+        /// Control alerta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl alerta;
+
+        /// <summary>
+        /// Control alerta2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl alerta2;
+
+        /// <summary>
+        /// Control tbNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbNombre;
+
+        /// <summary>
+        /// Control btnPrueba.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPrueba;
+
+        /// <summary>
+        /// Control btnPrueba2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPrueba2;
+    }
+}
